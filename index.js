c884c3eacc67f7c915d9eeb992df131da1e4f747
kathrin v <go.kathrin@gmail.com>
2018-05-14 00:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  array=[];
+  var array=[];
   for(i=0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays the ${instruments[i]}`);
   }
